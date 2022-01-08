e365697120c007c3d5600e6229d3362d1b9a1270
sinievanderben <sinievdben@live.nl>
2022-01-08 17:39:42 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from helperfunctions import *
 import pandas as pd
 
-## Get the data and split into train, val and test
+## Get the data, clean the data and split into train, val and test
 
 data = pd.read_csv("ship_data.csv", skiprows= 0)
 
@@ -11,3 +11,9 @@ y = data['gross_tonnage']
 X_train, X_val, X_test, y_train, y_val, y_test = getTrainValTest(X,y, 0.8, 0.1)
 
 ## Get additional useful features 
+
+## Get a suited algorithm
+
+## Tune the algorithm's hyperparameters 
+
+## Test the algorithm 
